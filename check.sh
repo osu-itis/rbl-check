c78fa9fd58063eebdfaab1ac085d2da468a227bf
Mike Akey <mike.akey@oregonstate.edu>
2017-05-03 14:02:01 -0700

    initial commit

---
+++ check.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./rbl.monitor --listfile rbllist smtp1.nws.oregonstate.edu smtp2.nws.oregonstate.edu smtp3.nws.oregonstate.edu relay1.nws.oregonstate.edu relay2.nws.oregonstate.edu relay3.nws.oregonstate.edu
